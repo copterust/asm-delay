6a8dfbcb780b4cf95f49d4a45f3b616e27531c50
Roma Sokolov <sokolov.r.v@gmail.com>
2019-03-17 14:29:14 +0100

    Add cycle to ms converter

--- src/lib.rs
+++ src/lib.rs
@@ -44,3 +44,39 @@ where
         delay(self.freq_base_us * us.into())
     }
 }
+
+/// Convert cycles back to time
+pub struct CyclesToTime {
+    freq_base_ms: f32,
+    freq_base_us: f32,
+}
+
+impl CyclesToTime {
+    /// Consturct new cycles to time converter of CPU frequency (Hertz)
+    pub fn new<F>(freq: F) -> Self
+    where
+        F: Into<Hertz<u32>>,
+    {
+        let freq_hz = freq.into().0;
+        CyclesToTime {
+            freq_base_ms: (freq_hz / 1_000) as f32,
+            freq_base_us: (freq_hz / 1_000_000) as f32,
+        }
+    }
+
+    /// Converts cycles to ms
+    pub fn to_ms<U>(&self, cycles: U) -> f32
+    where
+        U: Into<u32>,
+    {
+        self.freq_base_ms / (cycles.into() as f32)
+    }
+
+    /// Converts cycles to us
+    pub fn to_us<U>(&self, cycles: U) -> f32
+    where
+        U: Into<u32>,
+    {
+        self.freq_base_us / (cycles.into() as f32)
+    }
+}
